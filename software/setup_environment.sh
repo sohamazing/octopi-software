832d881a166fe47319fea3481c79b88320827d71
sohamazing <soham1202@gmail.com>
2024-04-09 21:11:01 -0700

    setup

--- software/setup_environment.sh
+++ software/setup_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Setup script for octopi-software environment
-#
-# Usage...
-#
+# -------------------------------------------------------
+# Usage
+# -------------------------------------------------------
 # brew install git (MacOS)
 # sudo apt-get install git (Linux)
 # 
@@ -13,79 +13,115 @@
 #
 # conda activate octopi
 # python main.py --simulation
+# -------------------------------------------------------
 
-# Function definitions
+# Function to compare version numbers
 version() {
     echo "$@" | awk -F. '{ printf("%d%03d%03d", $1,$2,$3); }'
 }
 
-check_cuda_nvidia() {
-    desired_cuda_version_major="12"  # Major version of desired CUDA
-    desired_driver_version="550"     # Desired NVIDIA driver version
+# Determines and installs the best NVIDIA driver, with checks for specific versions
+install_best_nvidia_driver() {
+    echo "Checking current NVIDIA driver version..."
+    local current_driver_version=$(nvidia-smi --query-gpu=driver_version --format=csv,noheader | cut -d '.' -f1 || echo "0")
 
-    # Extract current NVIDIA driver version
-    current_driver_version=$(nvidia-smi --query-gpu=driver_version --format=csv,noheader | cut -d '.' -f1)
-    echo "Current NVIDIA driver version: $current_driver_version"
-
-    # Attempt to extract current CUDA version
-    if command -v nvcc &> /dev/null; then
-        current_cuda_version=$(nvcc --version | grep "release" | awk '{print $6}' | cut -d ',' -f1)
-        current_cuda_version_major=$(echo $current_cuda_version | cut -d '.' -f1)
-    else
-        current_cuda_version_major="0"
+    if [[ "$current_driver_version" -eq 550 || "$current_driver_version" -eq 535 ]]; then
+        echo "Compatible NVIDIA driver version $current_driver_version is already installed."
+        return
     fi
-    echo "Current CUDA major version: $current_cuda_version_major"
-
-    # Compare versions
-    if [ $(version $current_driver_version) -lt $(version $desired_driver_version) ] || 
-       [ $(version $current_cuda_version_major) -ne $(version $desired_cuda_version_major) ]; then
-        return 1
+    
+    echo "No compatible NVIDIA driver version found. Determining the best driver for installation..."
+    # This example assumes ubuntu-drivers command is available and the system is Ubuntu
+    local recommended_driver=$(ubuntu-drivers devices | grep 'recommended' | grep 'nvidia-driver' | awk '{print $3}')
+    if [ -n "$recommended_driver" ]; then
+        echo "Purging existing NVIDIA drivers..."
+        sudo apt-get purge -y nvidia-*
+        echo "Installing recommended NVIDIA driver: $recommended_driver"
+        sudo apt-get install -y "$recommended_driver"
     else
-        return 0
+        echo "No recommended NVIDIA driver found by `ubuntu-drivers devices`. Considering manual installation."
+        # Manual installation of a driver if needed
+        # sudo apt-get install -y nvidia-driver-550     ### uncomment to install version 550 manually
     fi
 }
 
-install_cuda_linux() {
-    echo "System does not meet the desired requirements for CUDA or NVIDIA drivers. Installing CUDA..."
-    sudo apt-get update && sudo apt-get install -y cuda
+# Installs CUDA Toolkit if not already installed after ensuring the NVIDIA driver is correctly installed
+install_cuda_toolkit() {
+    if dpkg -l | grep -q cuda-12; then
+        echo "CUDA 12.x is already installed."
+        return
+    else
+        echo "Installing CUDA 12.x..."
+        if [ -f cuda-keyring_*.deb ]; then
+            echo "Removing existing CUDA keyring package..."
+            rm cuda-keyring_*.deb
+        fi
+        if dpkg -l | grep -q cuda-keyring; then
+            echo "Removing previously installed CUDA keyring..."
+            sudo dpkg --purge cuda-keyring
+        fi
+        wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+        sudo dpkg -i cuda-keyring_1.1-1_all.deb
+        rm cuda-keyring_1.1-1_all.deb
+        sudo apt-get install -y cuda-12
+        echo "CUDA 12.x installation completed."
+    fi
 }
 
+# Combines the steps to install CUDA and NVIDIA drivers
+install_cuda_nvidia_linux() {
+    if [[ "$(lsb_release -d)" =~ Ubuntu\ 22.04 ]]; then
+        sudo apt-get update
+        install_best_nvidia_driver
+        install_cuda_toolkit
+    else
+        echo "CUDA and NVIDIA driver installation skipped. This script is designed for Ubuntu 22.04."
+    fi
+}
+
+# Creates and activates a Conda environment if it doesn't already exist
 create_conda_env() {
-    env_name="octopi"
-    python_version="3.10"
+    local env_name="octopi"
+    local python_version="3.10"
 
-    echo "Creating conda environment '$env_name' with Python $python_version if it doesn't already exist..."
-    conda create -y -n $env_name python=$python_version || true
+    if conda info --envs | grep -qw "$env_name"; then
+        echo "Conda environment '$env_name' already exists. Activating it..."
+    else
+        echo "Creating conda environment '$env_name' with Python $python_version..."
+        conda create -y -n "$env_name" python="$python_version"
+    fi
     eval "$(conda shell.bash hook)"
-    conda activate $env_name
+    conda activate "$env_name"
+    echo "Conda environment '$env_name' activated."
 }
 
+# Installs required Python packages excluding PyTorch
 install_python_packages() {
-    echo "Updating pip and installing Python packages..."
-    pip install -U pip setuptools wheel
-    pip install -U numpy pandas scikit-learn
-    pip install -U PyQt5 pyqtgraph qtpy pyserial lxml==4.9.4 crc==1.3.0
-    pip install -U opencv-python-headless opencv-contrib-python-headless
-    pip install -U dask_image imageio aicsimageio tifffile 
-    pip install -U napari[all] napari-ome-zarr basicpy
+    echo "Installing Python packages..."
+    pip install -U pip setuptools wheel numpy pandas scikit-learn \
+                   PyQt5 pyqtgraph qtpy pyserial lxml==4.9.4 crc==1.3.0 \
+                   opencv-python-headless opencv-contrib-python-headless \
+                   dask_image imageio aicsimageio tifffile \
+                   napari[all] napari-ome-zarr basicpy
 }
 
-install_jax() {
+# Installs PyTorch with CUDA support for Linux using Conda, updates JAX to a specific version based on the OS
+install_conditional_packages() {
+    echo "Installing conditional packages..."
     case "$(uname -s)" in
         Linux*)
-            echo "Updating JAX with CUDA support for Linux..."
-            pip install -U 'jax[cuda12_pip]==0.4.23' --find-links https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+            pip install cuda-python cupy-cuda12x
+            conda install -y pytorch torchvision torchaudio cudatoolkit=12.1 -c pytorch -c nvidia
+            pip install 'jax[cuda12_pip]==0.4.23' --find-links https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
             ;;
         Darwin*)
-            echo "Updating JAX for CPU on macOS..."
-            pip install -U 'jax[cpu]==0.4.23'
-            ;;
-        *)
-            echo "Unsupported OS for specific JAX installation. Proceeding with general JAX installation."
+            pip install torch torchvision torchaudio 
+            pip install jax[cpu]==0.4.23
             ;;
     esac
 }
 
+# Installs the Galaxy Camera software
 install_galaxy_camera() {
     echo "Installing Galaxy Camera software..."
     cd drivers\ and\ libraries/daheng\ camera/Galaxy_Linux-x86_Gige-U3_32bits-64bits_1.2.1911.9122/
@@ -95,32 +131,11 @@ install_galaxy_camera() {
     python3 setup.py install
 }
 
-# Create and activate conda environment
+# Main script execution
 create_conda_env
-
-# Install Python packages
+install_cuda_nvidia_linux  # Check and install CUDA/NVIDIA drivers if necessary
 install_python_packages
-
-# Conditional CUDA installation and PyTorch setup on Linux
-case "$(uname -s)" in
-    Linux*)
-        if ! check_cuda_nvidia; then
-            install_cuda_linux
-            conda install -y pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
-        else
-            echo "CUDA and NVIDIA driver requirements are already satisfied."
-        fi
-        ;;
-    *)
-        echo "CUDA installation skipped for non-Linux systems."
-        conda install -y pytorch torchvision torchaudio -c pytorch
-        ;;
-esac
-
-# Install JAX
-install_jax
-
-# Install Galaxy Camera software
+install_conditional_packages  # Install PyTorch, JAX, and related CUDA packages
 install_galaxy_camera
 
 echo "Installation completed successfully."
